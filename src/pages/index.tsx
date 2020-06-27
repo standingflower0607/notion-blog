ee30ecdbe79040295c82035a762208f109d24f1b
立花冠 <tachibanakan@tachibanakannoMacBook-Pro.local>
2020-06-27 21:40:34 +0900

    modify design a bit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,10 +8,13 @@ import sharedStyles from '../styles/shared.module.css'
 export default () => (
   <>
     <Header titlePre="Home" />
-    <div className={sharedStyles.layout}>
+    <div className="container">
       <h1 className="hanko-font">Canned Bananas</h1>
       <h2>Kan Tachibana / 立花 冠</h2>
-      <p>夢がないので、平等な機会を作りたいです。それが夢かもしれません。</p>
+      <p>
+        夢がないので、平等な機会を作りたいです。それが夢かもしれません。そして、自分がそれで幸せを感じれれば十分ですわ。
+      </p>
+      <p className="dinosaur-font">ABCDEFGHJK</p>
 
       <h4>今の興味</h4>
       <p>テクノロジーの進歩と人間倫理</p>
