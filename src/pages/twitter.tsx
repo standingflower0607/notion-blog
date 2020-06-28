aa07dcc0f6474722ca3a59cd8e8da28258887886
立花冠 <tachibanakan@tachibanakannoMacBook-Pro.local>
2020-06-28 22:06:30 +0900

    enable to use bookmark

--- src/pages/twitter.tsx
+++ src/pages/twitter.tsx
@@ -1,17 +1,38 @@
 import Link from 'next/link'
 import Head from 'next/head'
 import Header from '../components/header'
+import Twitter from '../components/twitter'
 import ExtLink from '../components/ext-link'
 import Features from '../components/features'
 import GitHub from '../components/svgs/github'
 import sharedStyles from '../styles/shared.module.css'
 
+const tweets = [
+  {
+    username: null,
+    accountName: null,
+    userImage: null,
+    date: null,
+    content: null,
+    image: null,
+  },
+]
 export default () => (
   <>
     <Header titlePre="Twitter" />
     <div className="container">
       <h1>Twitter</h1>
       <p>Here is "my Twitter" 🤗, I don't have an account, though.</p>
+
+      <p>B : ....</p>
+      <p>
+        A : Oh, for real? It means it exploit our spare time and sell it to
+        advertiser, right?
+      </p>
+      <p>B : What are you saying? It's totally free.</p>
+      <p>A : So, How much does it cost?</p>
+      <p>B : Sure, You can keep up with current events.</p>
+      <p>A : Is there any merits to use Twitter ?</p>
     </div>
   </>
 )
