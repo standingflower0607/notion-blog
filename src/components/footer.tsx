593b963ff448e2abefc385a0585f1f258b2d192c
立花冠 <tachibanakan@tachibanakannoMacBook-Pro.local>
2020-07-09 20:34:54 +0900

    modified design

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,9 @@ import styles from '../styles/footer.module.css'
 export default () => (
   <>
     <footer className={styles.footer}>
-      <Link href={'/aboutBlog'}>このブログについて</Link>
+      <Link href={'/aboutBlog'}>
+        <p>このブログについて</p>
+      </Link>
       <p>&copy; Canned Bananas 2020 / 🧍🏻‍♀+ 🌸+ 👑 = 🍌</p>
     </footer>
   </>
