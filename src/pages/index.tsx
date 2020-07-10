e0abc15cceb9932d2330d765db90111043a9460c
立花冠 <tachibanakan@tachibanakannoMacBook-Pro.local>
2020-07-10 22:05:51 +0900

    bit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,17 +7,23 @@ import GitHub from '../components/svgs/github'
 import sharedStyles from '../styles/shared.module.css'
 
 export default () => (
-  <div className={sharedStyles.index}>
+  <>
     <Header titlePre="Home" />
-    <div className="container">
-      <img className={sharedStyles.img} src="/images/amekomi.jpg" alt="画像" />
-      <h1 className="utsukushi-font">Kan Tachibana / 立花 冠</h1>
-      <p className="utsukushi-font">
-        夢がないので、平等な機会を作りたいです。それが夢かもしれません。そして、自分がそれで幸せを感じれれば十分ですわ。
-      </p>
+    <div className={sharedStyles.index}>
+      <div className="container">
+        <img
+          className={sharedStyles.img}
+          src="/images/amekomi.jpg"
+          alt="画像"
+        />
+        <h1 className="utsukushi-font">Kan Tachibana / 立花 冠</h1>
+        <p className="utsukushi-font">
+          夢がないので、平等な機会を作りたいです。それが夢かもしれません。そして、自分がそれで幸せを感じれれば十分ですわ。
+        </p>
 
-      <h4>今の興味</h4>
-      <p>テクノロジーの進歩と人間倫理</p>
+        <h4>今の興味</h4>
+        <p>テクノロジーの進歩と人間倫理</p>
+      </div>
     </div>
-  </div>
+  </>
 )
