ed8d8eae47c5475d90c690b2bd0ba778fa55c382
立花冠 <tachibanakan@tachibanakannoMacBook-Pro.local>
2020-07-26 18:53:52 +0900

    changed footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,26 +7,18 @@ export default () => (
     <footer className={styles.footer}>
       <div className={styles.footerWrapper}>
         <div className={styles.footerRight}>
-          <div className={styles.footerProfile}>
-            <img src="/images/for_insta_compressed.jpg" alt="" />
-          </div>
+          <div className={styles.footerProfile}></div>
         </div>
         <div className={styles.footerLeft}>
-          <div className={styles.footerCategory}>
-            <h3>Cagegory</h3>
-            <p>ちょいお待ち</p>
-          </div>
-          <div className={styles.footerLink}>
-            <h3>Link</h3>
-            <p>ちょいお待ち</p>
-          </div>
+          <div className={styles.footerCategory}></div>
+          <div className={styles.footerLink}></div>
         </div>
       </div>
       <hr />
+      <p>&copy; Canned Bananas 2020 / 🧍🏻‍♀ + 🌸 + 👑 = 🍌 </p>
       <p>
         Powerd by <a href="https://github.com/ijjk/notion-blog">Notion-blog</a>
       </p>
-      <p>&copy; Canned Bananas 2020 / 🧍🏻‍♀ + 🌸 + 👑 = 🍌 </p>
     </footer>
   </>
 )
